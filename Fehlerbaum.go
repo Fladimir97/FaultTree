1f04c1460a341673d42787054ec654f896afdf6f
Fabian Flad <108148154+Fladimir97@users.noreply.github.com>
2026-07-08 09:31:23 +0200

    Add files via upload

--- Fehlerbaum.go
+++ Fehlerbaum.go
@@ -66,7 +66,7 @@ func (n AND_NODE) getFailure() float64 {
 }
 
 func (n AND_NODE) toDot() string {
-	var out string = fmt.Sprintf("\n%s [shape=house, label=\"%s\"]", n.getTitle(), n.getLabel())
+	var out string = fmt.Sprintf("\n%s [shape=none, label=%s]", n.getTitle(), n.getLabel())
 	for _, child := range n.Children {
 		out += fmt.Sprintf("\n%s -> %s", n.getTitle(), child.getTitle())
 		out += child.toDot()
@@ -83,7 +83,16 @@ func (n AND_NODE) toDigraph() string {
 }
 
 func (n AND_NODE) getLabel() string {
-	return fmt.Sprintf("%s\nF: %.4f\nR: %.4f", n.getTitle(), n.getFailure(), n.getReliability())
+	return fmt.Sprintf(`<<TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0">
+        <TR><TD>%s</TD></TR>
+        <TR><TD><IMG SRC="images/and_gate.png"/></TD></TR>
+        <TR><TD>F: %.4f</TD></TR>
+        <TR><TD>R: %.4f</TD></TR>
+    </TABLE>>`, n.Title, n.getFailure(), n.getReliability())
+}
+
+func newAND_NODE(title string) *AND_NODE {
+	return &AND_NODE{title, []NODE{}}
 }
 
 // ---- OR NODE ------
@@ -132,7 +141,7 @@ func (n OR_NODE) getTitle() string {
 }
 
 func (n OR_NODE) toDot() string {
-	var out string = fmt.Sprintf("\n%s [shape=invhouse, label=\"%s\"]", n.getTitle(), n.getLabel())
+	var out string = fmt.Sprintf("\n%s [shape=none, label=%s]", n.getTitle(), n.getLabel())
 	for _, child := range n.Children {
 		out += fmt.Sprintf("\n%s -> %s", n.getTitle(), child.getTitle())
 		out += child.toDot()
@@ -145,7 +154,16 @@ func (n OR_NODE) toDigraph() string {
 }
 
 func (n OR_NODE) getLabel() string {
-	return fmt.Sprintf("%s\nF: %.4f\nR: %.4f", n.getTitle(), n.getFailure(), n.getReliability())
+	return fmt.Sprintf(`<<TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0">
+        <TR><TD>%s</TD></TR>
+        <TR><TD><IMG SRC="images/or_gate.png"/></TD></TR>
+        <TR><TD>F: %.4f</TD></TR>
+        <TR><TD>R: %.4f</TD></TR>
+    </TABLE>>`, n.Title, n.getFailure(), n.getReliability())
+}
+
+func newOR_NODE(title string) *OR_NODE {
+	return &OR_NODE{title, []NODE{}}
 }
 
 // ---- EVENT NODE ------
@@ -180,30 +198,36 @@ func (n EVENT) getLabel() string {
 	return fmt.Sprintf("%s\nF: %.4f\nR: %.4f", n.getTitle(), n.getFailure(), n.getReliability())
 }
 
+func newEvent(title string, reliability, failure float64) (*EVENT, error) {
+	if reliability+failure != 1 {
+		return nil, fmt.Errorf("Fehler: Reliability und Failure müssen in der Summe 1 ergeben")
+	}
+	return &EVENT{title, reliability, failure}, nil
+}
+
 // ---- main Func ------
 
 func main() {
-	AND := AND_NODE{
-		"TOP", []NODE{},
-	}
-	OR := OR_NODE{
-		"OR1", []NODE{},
-	}
-	E1 := EVENT{
-		"E1", 0.99, 0.01,
-	}
-	E2 := EVENT{
-		"E2", 0.98, 0.02,
-	}
-	E3 := EVENT{
-		"E3", 0.97, 0.03,
-	}
-	AND.addNode(&OR)
-	AND.addEvent(E1)
-	OR.addEvent(E2)
-	OR.addEvent(E3)
+	TOP := newOR_NODE("TOP")
+	A1 := newOR_NODE("A")
+	A2 := newAND_NODE("B")
+	E1, _ := newEvent("E1", 0.99, 0.01)
+	E2, _ := newEvent("E2", 0.98, 0.02)
+	E3, _ := newEvent("E3", 0.995, 0.005)
+	E4, _ := newEvent("E4", 0.95, 0.05)
+	E5, _ := newEvent("E5", 0.99, 0.01)
 
-	MCS := AND.getCutSets()
+	TOP.addNode(A1)
+	TOP.addNode(A2)
+
+	A1.addEvent(*E1)
+	A1.addEvent(*E2)
+	A1.addEvent(*E3)
+
+	A2.addEvent(*E4)
+	A2.addEvent(*E5)
+
+	MCS := TOP.getCutSets()
 	// fmt.Print(MCS)
 	for index, cutset := range MCS {
 		fmt.Printf("\n C_%d := {", index+1)
@@ -212,8 +236,8 @@ func main() {
 		}
 		fmt.Print("}")
 	}
-	fmt.Printf("\nZuverlässigkeit des Gesamtsystems:\t%.5f", AND.getReliability())
-	fmt.Printf("\nAusfallwahrscheinlichkeit des Gesamtsystems:\t%.5f", AND.getFailure())
-	var digraph string = AND.toDigraph()
+	fmt.Printf("\nZuverlässigkeit des Gesamtsystems:\t%.5f", TOP.getReliability())
+	fmt.Printf("\nAusfallwahrscheinlichkeit des Gesamtsystems:\t%.5f", TOP.getFailure())
+	var digraph string = TOP.toDigraph()
 	os.WriteFile("Fehlerbaum.dot", []byte(digraph), 0644)
 }
